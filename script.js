15f14518f59b40993b4900344bbee84282f52822
Griezman2003 <garciagomezgamaliel@gmail.com>
2024-06-14 20:11:20 -0600

    uupdate responsive

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const icon = document.querySelector('.menu');
-const menuList = document.querySelector('.menu ul');
 
-icon.addEventListener('click', function() {
-  menuList.classList.toggle('active');
+document.addEventListener('DOMContentLoaded', function() {
+  const menuButton = document.querySelector('.menu-button');
+  const menuList = document.querySelector('.menu');
+
+  menuButton.addEventListener('click', function() {
+      menuList.classList.toggle('show');
+  });
+
+  // Opcional: Cerrar el menú al hacer clic fuera de él
+  document.addEventListener('click', function(event) {
+      if (!menuButton.contains(event.target) && !menuList.contains(event.target)) {
+          menuList.classList.remove('show');
+      }
+  });
 });
